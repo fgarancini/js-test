2bf62231bcc860d2cf9626624d8566a5eafd9fb6
Franco Garancini <f.garancini@hotmail.com>
2021-02-10 23:27:17 -0300

    bigWip

--- db-contacts-users/client-data/index.js
+++ db-contacts-users/client-data/index.js
@@ -1,25 +1,27 @@
 const form = document.getElementById("form");
 const API_URL = "http://127.0.0.1:2001/contactos";
 
-form.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    const formData = new FormData(form);
-    
-    const password = formData.get("password");
-    const username = formData.get("login");
-    
-    const user = 
-    {
-        username,
-        password
-    };
-    console.log(user);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
 
-    fetch(API_URL,{
-        method:'POST',
-        body: JSON.stringify(user),
-        headers: {'content-type':'application/json'}
-    }).then(usuario =>{
-        console.log(usuario);
-    }).catch(err => console.log(err));
+  const password = formData.get("password");
+  const username = formData.get("login");
+
+  const user = {
+    username,
+    password,
+  };
+  console.log(user);
+
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: { "content-type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((usuario) => {
+      console.log(usuario);
+    })
+    .catch((err) => console.log(err));
 });
--- db-contacts-users/server-data/app.js
+++ db-contacts-users/server-data/app.js
@@ -1,34 +1,56 @@
-const express = require("express");
-const cors = require("cors");
-const monk = require("monk");
+/**
+ * Express es la funcion que trae todos los metodos 
+ * de esta infraestructura http y middleware
+ * 
+ * Middleware: es un bloque de codigo que se ejecuta ENTRE 
+ * el req hasta que la peticion llegue al servidor
+ */
+
+
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
 const app = express();
 const port = 2001;
-const db = monk("localHost:27017/agenda");
-const usuarios = db.get("contactos");
+const db = monk('localHost:27017/agenda');
+const usuarios = db.get('contactos');
 
 app.use(cors());
 
 app.use(express.json());
-app.enable('trust proxy');
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Esperando los contactos....",
-  });
-});
-app.post("/contactos", (req, res) => {
-  const user = {
-    username: req.body.username.toString(),
-    passwd: req.body.passwd.toString(),
-    created: new Date(),
-  };
-  console.log(user);
-
-  usuarios.insert(user).then((usuario) => {
-    res.json(usuarios);
-  });
-});
 // app.enable('trust proxy');
+
+autenticacionValida = (user) =>{
+  return user.username && user.username.toString().trim() != '' 
+  && user.password && user.password.toString().trim() != '';
+}
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Esperando los contactos....',
+  });
+});
+app.post('/contactos', (req, res) => {
+  if (autenticacionValida(req.body)) {
+    const user = {
+      username: req.body.username.toString(),
+      passwd: req.body.password.toString(),
+      created: new Date(),
+    };
+    console.log(user);
+
+    usuarios.insert(user).then(usuarios => {
+      res.json(usuarios);
+    });
+    }
+    else 
+    {
+      res.status(402);
+        res.json({
+            message:'Name and spaces are required'
+        })
+    }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
